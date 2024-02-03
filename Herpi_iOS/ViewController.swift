82c33d9f7a40948b18ec97f731927cf9de257484
Konstantine Tsirgvava <koka.wirgvava@gmail.com>
2024-02-03 11:45:00 +0400

    Initial Commit

---
+++ Herpi_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Herpi_iOS
+//
+//  Created by Konstantine Tsirgvava on 03.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
