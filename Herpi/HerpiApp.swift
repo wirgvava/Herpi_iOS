22747a79a49a98535623da8a628e9da80cff1e56
Konstantine Tsirgvava <koka.wirgvava@gmail.com>
2025-06-09 17:16:35 +0400

    initial commit

---
+++ Herpi/HerpiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerpiApp.swift
+//  Herpi
+//
+//  Created by Konstantine Tsirgvava on 09.06.25.
+//
+
+import SwiftUI
+
+@main
+struct HerpiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
